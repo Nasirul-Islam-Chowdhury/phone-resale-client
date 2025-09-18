787295560183d140ba16e1bdbcff922b7045a104
MD NASIRUL ISLAM CHOWDHURY <nasirchy252@gmail.com>
2025-09-18 06:56:02 +0600

    card design fix 2

--- src/Components/AdvertisedItems/AdvertisedItems.jsx
+++ src/Components/AdvertisedItems/AdvertisedItems.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
+import React, { Profiler, useEffect, useState } from "react";
 import Loading from "../../SharedComponents/Loading/Loading";
 import { Link } from "react-router-dom";
 import AOS from 'aos';
@@ -23,18 +23,18 @@ const AdvertisedItems = () => {
       {finalPro.length>0 && (
         <div data-aos="fade-right" className="container text-black font-primary">
           <h2 className="text-3xl font-semibold lg:p-4 my-8 lg:text-start text-center">Advertised Products</h2>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+          <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
             {finalPro.map((product) => (
               <div
                 key={product._id}
-                className="card  card-compact lg:w-96 rounded-sm mx-auto shadow-xl"
+                className="card  card-compact  max-w-[25rem] xl:max-w-[20rem] w-full rounded-sm mx-auto shadow-xl"
               >
                 <figure>
-                  <img className=" h-60 object-cover"  src={product.images[0]} alt="Shoes" />
+                  <img className=" h-60 w-full object-cover"  src={product.images[0]} alt="Shoes" />
                 </figure>
                 <div className="card-body">
                   <h2 className="card-title">{product.name}</h2>
-                  <p>{product.description.slice(0, 100)}...</p>
+                  <p title={product.description}>{product.description.slice(0, 100)}...</p>
                   <h2 className="card-title">Price: ${product.price}</h2>
                   <div className="card-actions justify-end">
                   <button  className=" w-24 h-8 rounded bg-[#379389] text-white  font-bold mt-4">
